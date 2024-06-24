89b5653528c6c365835056736c6db2560126af20
pseudo-ankit <ankit.singh119219@gmail.com>
2024-06-24 12:14:31 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.getTransaction().commit();
+    }
+}
