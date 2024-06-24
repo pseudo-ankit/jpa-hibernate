26fec68a242134fe500db18016013b2c4b12e393
pseudo-ankit <ankit.singh119219@gmail.com>
2024-06-24 18:16:59 +0530

    persist using persistence xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,23 @@ package org.example;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.example.entities.Product;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            Product p = new Product();
+            p.setId(1L);
+            p.setName("One");
+
+            em.persist(p);
+            em.getTransaction().commit();
+        }finally {
+            em.close();
+        }
     }
 }
